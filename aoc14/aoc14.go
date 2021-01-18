69eb0e3815e70c77a9701202beff26b27b4cc22f
Arthur_Dayne <xavierivan2000@hotmail.com>
2021-01-17 22:12:29 -0500

    solved day 14 part1

---
+++ aoc14/aoc14.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	/*AOC day 14. solution by sirArthurDayne */
+	inputData, err := scanLines("data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+    //PART1
+    fmt.Println("total",parseBlocks(inputData))
+    //PART2.
+}
+
+func scanLines(path string) ([]string, error) {
+
+  file, err := os.Open(path)
+  if err != nil {
+     return nil, err
+  }
+
+  defer file.Close()
+
+  scanner := bufio.NewScanner(file)
+
+  scanner.Split(bufio.ScanLines)
+
+  var lines []string
+
+  for scanner.Scan() {
+    lines = append(lines, scanner.Text())
+  }
+
+  return lines, nil
+}
+
+func parseBlocks(data []string) int {
+    mem := make(map[int]int)
+    currentMask := ""
+    for _, line := range data {
+        if strings.Contains(line, "mask") {
+            currentMask = strings.Trim(line, "mask = ")
+        } else if strings.Contains(line, "mem") {
+            memSplit := strings.Split(line," = ")
+            //get key and value int
+            key := StringToInt(strings.Trim(memSplit[0], "mem[]"))
+            valStringToInt := StringToInt(strings.TrimSpace(memSplit[1]))
+            //cast value from string to Binary 36bits
+            valIntToBinary := padStart(IntToBinary(int64(valStringToInt)),36,"0")
+            //Apply the mask
+            newBinaryValue := applyMask(valIntToBinary, currentMask)
+            //cast back from Binary to Int
+            value := BinaryToInt(newBinaryValue)
+            //save in map
+            mem[key] = int(value)
+        }
+    }
+
+    //add values and return
+    total := 0
+    for _,val := range mem {
+        total += val
+    }
+    return total
+}
+
+func StringToInt(n string) int {
+    val, err := strconv.Atoi(n)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return val
+}
+
+func IntToBinary(n int64) string {
+    return strconv.FormatInt(n,2)
+}
+
+func BinaryToInt(n string) int64 {
+    val,err :=  strconv.ParseInt(n, 2, 64)
+    if err != nil {
+        log.Fatal(err)
+    }
+    return val
+}
+
+func padStart(n string, length int, char string) string {
+    container :=  ""
+    for i:=0; i < length; i++ {
+        container+=char
+    }
+    outputVal := container + n
+    return outputVal[len(outputVal)-length:]
+}
+
+func applyMask(val, currentMask string) string {
+    output:= ""
+    for i:=0; i < len(currentMask); i++ {
+        if currentMask[i] == 'X' {
+            output += string(val[i])
+        } else if currentMask[i] == '1' {
+            output += "1"
+        } else if currentMask[i] == '0' {
+            output += "0"
+        }
+    }
+    return output
+}
