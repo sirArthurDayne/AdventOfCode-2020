dafab516048f0594b1b45e4e705d7d7ecd65a48d
Arthur_Dayne <xavierivan2000@hotmail.com>
2021-08-27 23:37:52 -0500

    small changes to aoc14

--- aoc14/aoc14.go
+++ aoc14/aoc14.go
@@ -11,13 +11,16 @@ import (
 
 func main() {
 	/*AOC day 14. solution by sirArthurDayne */
-	inputData, err := scanLines("data.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
+	// inputData, err := scanLines("data.txt")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
     //PART1
-    fmt.Println("total",parseBlocks(inputData))
+    //fmt.Println("total",getSumOfAdressesInMemory(inputData))
     //PART2.
+    // fmt.Println("total",getSumOfAdressesInMemory2(inputData))
+    list := getAllPosiblesCombinations("1XX0")
+    fmt.Println(list)
 }
 
 func scanLines(path string) ([]string, error) {
@@ -42,7 +45,8 @@ func scanLines(path string) ([]string, error) {
   return lines, nil
 }
 
-func parseBlocks(data []string) int {
+//PART 1
+func getSumOfAdressesInMemory(data []string) int {
     mem := make(map[int]int)
     currentMask := ""
     for _, line := range data {
@@ -69,6 +73,35 @@ func parseBlocks(data []string) int {
     for _,val := range mem {
         total += val
     }
+    return total
+}
+
+//PART 2
+func getSumOfAdressesInMemory2(data []string) int {
+    mem := make(map[int]int)
+    currentMask := ""
+    for _, line := range data {
+        if strings.Contains(line, "mask") {
+            currentMask = strings.Trim(line, "mask = ")
+        } else if strings.Contains(line, "mem") {
+            memSplit := strings.Split(line," = ")
+            //get key and value int
+            key := StringToInt(strings.Trim(memSplit[0], "mem[]"))
+            valStringToInt := StringToInt(strings.TrimSpace(memSplit[1]))
+            //cast value from string to Binary 36bits
+            valIntToBinary := padStart(IntToBinary(int64(valStringToInt)),36,"0")
+
+            //apply mask rules
+            valMask := applyMask2(valIntToBinary, currentMask)
+            //GetAllAddresses
+
+            //add result to mem[key]
+            mem[key] = getAllPosiblesCombinations(valMask)
+        }
+    }
+
+    //sum all in memory
+    total:=0
     return total
 }
 
@@ -114,3 +147,22 @@ func applyMask(val, currentMask string) string {
     }
     return output
 }
+
+func applyMask2(val, currentMask string) string {
+    output:= ""
+    for i:=0; i < len(currentMask); i++ {
+        if currentMask[i] == 'X' {
+            output += "X"
+        } else if currentMask[i] == '1' {
+            output += "1"
+        } else if currentMask[i] == '0' {
+            output += string(val[i])
+        }
+    }
+    return output
+}
+
+func getAllPosiblesCombinations(value string) int {
+return -1
+}
+
